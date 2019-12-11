3fadab2fbe5810dfe8c7ae5525e62ee90c8d964d
Boido <f.boido08@gmail.com>
2019-12-11 17:09:27 +0100

    Retrieving a test variable in Init() in order to check the ads-connection

--- PlcMqttClient/src/packageAds/PlcConnector.java
+++ PlcMqttClient/src/packageAds/PlcConnector.java
@@ -22,16 +22,91 @@ import packageSystem.StateMachine;
 
 public class PlcConnector extends StateMachine {
 	
+	long err;
+	AmsAddr addr;
+	JNIByteBuffer handleBuff,symbolBuff,dataBuff;
+	
+	private static final String plcConnected = "ADS.fbAdsConnector.cbConnected";
+	
 	public PlcConnector()
 	{
 		super();
-		Start();
+		
 	}
 
 	@Override
 	protected void Init() {
-		// TODO Auto-generated method stub
 		
+		addr = new AmsAddr();
+		handleBuff = new JNIByteBuffer(Integer.SIZE / Byte.SIZE);
+		symbolBuff = new JNIByteBuffer(plcConnected.getBytes());
+		dataBuff = new JNIByteBuffer(1);
+		
+		//Open communication
+		AdsCallDllFunction.adsPortOpen();
+		err = AdsCallDllFunction.getLocalAddress(addr);
+		addr.setPort(851);
+		
+		if(err != 0)
+		{
+			System.out.println("Error: Open communication: 0x" + Long.toHexString(err));
+			return;
+		}
+		else
+		{
+			System.out.println("Success: Open communication!");
+		}
+		
+		err = AdsCallDllFunction
+				.adsSyncReadWriteReq
+				(
+					addr,
+					AdsCallDllFunction.ADSIGRP_SYM_HNDBYNAME,
+					0x0,
+					handleBuff.getUsedBytesCount(),
+                	handleBuff,
+                	symbolBuff.getUsedBytesCount(),
+                	symbolBuff
+				);
+		
+		if(err!=0) 
+		{ 
+		    System.out.println("Error: Get handle: 0x" + Long.toHexString(err)); 
+		    AdsCallDllFunction.adsPortClose();
+		    return;
+		} 
+		else 
+		{
+		    System.out.println("Success: Get handle!");
+		}
+		
+		// Handle: byte[] to int
+		int hdlBuffToInt = Convert.ByteArrToInt(handleBuff.getByteArray());
+		      
+		// Read value by handle
+		err = AdsCallDllFunction
+				.adsSyncReadReq
+				(
+					addr,
+	                AdsCallDllFunction.ADSIGRP_SYM_VALBYHND,
+	                hdlBuffToInt,
+	                0x4,
+	                dataBuff
+                );
+		
+		if(err!=0)
+		{
+			System.out.println("Error: Read by handle: 0x" + Long.toHexString(err));
+			return;
+		}
+		else
+		{
+			// Data: byte[] to boolean
+			boolean val = Convert.ByteArrToBool(dataBuff.getByteArray());
+			System.out.println("Success: PLCVar value: " + val);
+			Start();
+		}
+
 	}
 
 	@Override
@@ -49,7 +124,7 @@ public class PlcConnector extends StateMachine {
 	@Override
 	protected void Busy() {
 		// TODO Auto-generated method stub
-		
+		//AdsCallDllFunction.adsSyncReadWriteReq() 
 	}
 
 	@Override
