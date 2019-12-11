1194d94cd1b4e2c831ec78d311e2aee6a20ccf2c
Boido <f.boido08@gmail.com>
2019-12-11 15:34:26 +0100

    plcConnector executed before entering the while loop.

--- PlcMqttClient/src/packageMain/Main.java
+++ PlcMqttClient/src/packageMain/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PlcConnector plcConnector = new PlcConnector();
+		plcConnector.Execute();
 		while(true)
 		{
 			plcConnector.checkStateMachine();
